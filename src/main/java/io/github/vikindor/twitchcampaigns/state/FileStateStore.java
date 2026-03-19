3b82d7600287760f38f4608b59fa8ee0bade9ee8
Vikindor <vikindor@protonmail.com>
2026-03-19 16:36:20 +0300

    refactor: replace file campaign cache store with generic file state store

--- src/main/java/io/github/vikindor/twitchcampaigns/state/FileStateStore.java
+++ src/main/java/io/github/vikindor/twitchcampaigns/state/FileStateStore.java
@@ -1,36 +1,41 @@
-package io.github.vikindor.twitchcampaigns.cache;
+package io.github.vikindor.twitchcampaigns.state;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Supplier;
 
-public final class FileCampaignCacheStore implements CampaignCacheStore {
+public final class FileStateStore<T> implements StateStore<T> {
     private final ObjectMapper objectMapper;
     private final Path path;
+    private final Class<T> type;
+    private final Supplier<T> emptyStateFactory;
 
-    public FileCampaignCacheStore(ObjectMapper objectMapper, Path path) {
+    public FileStateStore(ObjectMapper objectMapper, Path path, Class<T> type, Supplier<T> emptyStateFactory) {
         this.objectMapper = objectMapper;
         this.path = path;
+        this.type = type;
+        this.emptyStateFactory = emptyStateFactory;
     }
 
     @Override
-    public CacheState load() throws IOException {
+    public T load() throws IOException {
         if (Files.notExists(path)) {
-            return CacheState.empty();
+            return emptyStateFactory.get();
         }
 
         String content = Files.readString(path);
         if (content.isBlank()) {
-            return CacheState.empty();
+            return emptyStateFactory.get();
         }
 
-        return objectMapper.readValue(content, CacheState.class);
+        return objectMapper.readValue(content, type);
     }
 
     @Override
-    public void save(CacheState state) throws IOException {
+    public void save(T state) throws IOException {
         Path parent = path.getParent();
         if (parent != null) {
             Files.createDirectories(parent);
